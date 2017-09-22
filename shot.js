b4341d8ed0a969caaa8dabd8c09202dfc2870197
andreilupu <andrei.lupu@pixelgrade.com>
2017-09-22 13:49:35 +0300

    try to output as image data to json

--- shot.js
+++ shot.js
@@ -29,8 +29,12 @@ const argv = require('yargs')
 	.option('before_ss', {
 		describe: 'A script file loaded before taking the screenshot'
 	})
-	.demandOption(['url'])
+	.option('configPath', {
+		describe: 'A script file loaded before taking the screenshot'
+	})
 	.argv
+
+const { configPath } = argv
 
 const screens = {
 	"mobile" : {
@@ -47,59 +51,86 @@ const screens = {
 	}
 }
 
-const {url, screen, out, delay, css, style, width, height, before_ss} = argv
-
 const sleep = (ms) => {
 	return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-(async () => {
-	const browser = await puppeteer.launch({
-		args: [
-			'--no-sandbox',
-			'--disable-gpu',
-			'--disable-setuid-sandbox',
-			'--remote-debugging-port=9222'
-		]
-	})
+function shot(url, title, screen = 'desk', out = '-', delay = 1500, before_ss = null){
+	puppeteer.launch().then(async browser => {
+		console.log('shot a ss')
 
-	const sizes = screens[screen]
+		const sizes = screens[screen]
+		// console.log(browser);
+		const page = await browser.newPage()
+		// return sizes;
 
-	const page = await browser.newPage()
+		await page.setViewport({'width': sizes.w, 'height': sizes.h, deviceScaleFactor: 1 })
 
-	await page.setViewport({'width': sizes.w, 'height': sizes.h, deviceScaleFactor: 1 })
+		await page.goto(url)
 
-	await page.goto(url)
+		await sleep(delay)
 
-	await sleep(delay)
-
-	if ( typeof before_ss !== "undefined" ) {
-		try {
-			const bf = require(before_ss)
-			bf(page)
-		} catch (e) {
-			console.log(e);
+		if ( typeof before_ss !== "undefined" && before_ss !== null ) {
+			try {
+				const bf = require(before_ss)
+				bf(page)
+			} catch (e) {
+				console.log(e);
+			}
 		}
-	}
 
-	var deviceDimentions = await page._client.send('Page.getLayoutMetrics');
+		var deviceDimentions = await page._client.send('Page.getLayoutMetrics');
 
-	let ssArgs = {
-		type: 'jpeg',
-		quality: 70,
-		clip: {
-			x: 0,
-			y: 0,
-			width: sizes.w,
-			height: deviceDimentions.contentSize.height
+		let ssArgs = {
+			type: 'jpeg',
+			quality: 70,
+			clip: {
+				x: 0,
+				y: 0,
+				width: sizes.w,
+				height: deviceDimentions.contentSize.height
+			}
 		}
-	}
 
-	if ( out !== '-' ) {
-		ssArgs.path = out + '.jpeg';
-	}
+		if ( out.indexOf('.json') ) {
+			delete ssArgs.type
+			delete ssArgs.quality
 
-	await page.screenshot(ssArgs);
+			let imgJson = {
+				title: title,
+				img: await page.screenshot(ssArgs)
+			}
 
-	browser.close();
-})()
+
+			await fs.writeFile( './' + out,  JSON.stringify(imgJson), 'utf8', function () {
+				return null;
+			});
+
+		} else if ( out !== '-' ) {
+			ssArgs.path = out + '.jpeg';
+			await page.screenshot(ssArgs);
+		}
+
+		console.log('should close')
+		browser.close();
+	});
+}
+
+if ( typeof configPath === "undefined" ) {
+	const {url, screen, out, delay } = argv
+	shot(url, out, screen, out, delay)
+} else {
+	var config = fs.readFileSync(configPath, 'utf-8')
+	config = JSON.parse(config)
+
+	Object.keys( config ).forEach( key => {
+		let pages = config[key];
+
+		Object.keys( pages ).forEach( pageName => {
+			let args = pages[pageName]
+			setTimeout( function () {
+				shot( args.url, pageName, args.screen, args.out )
+			}, 200)
+		})
+	});
+}
