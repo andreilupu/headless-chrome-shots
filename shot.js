e3d2c0d20a867e2df1eb54c077143b718f605b91
andreilupu <andrei.lupu@pixelgrade.com>
2017-09-22 16:28:26 +0300

    hacking promises ...not fun

--- shot.js
+++ shot.js
@@ -34,10 +34,10 @@ const argv = require('yargs')
 	})
 	.argv
 
-const { configPath } = argv
+const {configPath} = argv
 
 const screens = {
-	"mobile" : {
+	"mobile": {
 		"w": 400,
 		"h": 600
 	},
@@ -55,8 +55,7 @@ const sleep = (ms) => {
 	return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-function shot(url, title, screen = 'desk', out = '-', delay = 1500, before_ss = null){
-	puppeteer.launch({
+puppeteer.launch({
 		args: [
 			'--no-sandbox',
 			'--disable-gpu',
@@ -64,72 +63,84 @@ function shot(url, title, screen = 'desk', out = '-', delay = 1500, before_ss = 
 			'--remote-debugging-port=9222'
 		]
 	}).then(async browser => {
-		const sizes = screens[screen]
-		const page = await browser.newPage()
-		await page.setViewport({'width': sizes.w, 'height': sizes.h, deviceScaleFactor: 1 })
-		await page.goto(url)
-		await sleep(delay)
 
-		if ( typeof before_ss !== "undefined" && before_ss !== null ) {
-			try {
-				const bf = require(before_ss)
-				bf(page)
-			} catch (e) {
-				console.log(e);
+		const shot = async function (url, title, screen = 'desk', out = '-', delay = 1500, before_ss = null, last = false ) {
+			const sizes = screens[screen]
+			const page = await browser.newPage()
+			await page.setViewport({'width': sizes.w, 'height': sizes.h, deviceScaleFactor: 1})
+			await page.goto(url)
+			await sleep(delay)
+
+			if (typeof before_ss !== "undefined" && before_ss !== null) {
+				try {
+					const bf = require(before_ss)
+					bf(page)
+				} catch (e) {
+					console.log(e);
+				}
+			}
+
+			var deviceDimentions = await page._client.send('Page.getLayoutMetrics');
+
+			let ssArgs = {
+				type: 'jpeg',
+				quality: 70,
+				clip: {
+					x: 0,
+					y: 0,
+					width: sizes.w,
+					height: deviceDimentions.contentSize.height
+				}
+			}
+
+			if (out.indexOf('.json')) {
+				delete ssArgs.type
+				delete ssArgs.quality
+
+				let imgJson = {
+					title: title,
+					img: await page.screenshot(ssArgs)
+			}
+
+				await fs.writeFile('./' + out, JSON.stringify(imgJson), 'utf8', function () {
+					return null;
+				});
+
+			} else if (out !== '-') {
+				ssArgs.path = out + '.jpeg';
+				await page.screenshot(ssArgs);
+			}
+
+			if ( last ) {
+				// console.log('should close')
+				browser.close()
 			}
 		}
 
-		var deviceDimentions = await page._client.send('Page.getLayoutMetrics');
+		if (typeof configPath === "undefined") {
+			const {url, screen, out, delay} = argv
+			await shot(url, out, screen, out, delay)
+		} else {
+			var config = fs.readFileSync(configPath, 'utf-8')
+			config = JSON.parse(config)
 
-		let ssArgs = {
-			type: 'jpeg',
-			quality: 70,
-			clip: {
-				x: 0,
-				y: 0,
-				width: sizes.w,
-				height: deviceDimentions.contentSize.height
-			}
-		}
+			// await shot( 'https://andrei-lupu.com', 'page', 'desk', 'page.json' );
 
-		if ( out.indexOf('.json') ) {
-			delete ssArgs.type
-			delete ssArgs.quality
+			Object.keys(config).forEach( function(key) {
+				let pages = config[key];
+				var last = false
 
-			let imgJson = {
-				title: title,
-				img: await page.screenshot(ssArgs)
-			}
+				Object.keys(pages).forEach( function( pageName, k ) {
 
-			await fs.writeFile( './' + out,  JSON.stringify(imgJson), 'utf8', function () {
-				return null;
+					let args = pages[pageName];
+
+					if ( ( Object.keys(pages).length - 1) === k) {
+						last = true
+					}
+
+					shot( args.url, pageName, args.screen, args.out, 1000, null, last )
+				})
 			});
-
-		} else if ( out !== '-' ) {
-			ssArgs.path = out + '.jpeg';
-			await page.screenshot(ssArgs);
 		}
 
-		console.log('should close')
-		browser.close();
-	});
-}
-
-if ( typeof configPath === "undefined" ) {
-	const {url, screen, out, delay } = argv
-	shot(url, out, screen, out, delay)
-} else {
-	var config = fs.readFileSync(configPath, 'utf-8')
-	config = JSON.parse(config)
-
-	Object.keys( config ).forEach( key => {
-		let pages = config[key];
-
-		Object.keys( pages ).forEach( pageName => {
-			let args = pages[pageName]
-			setTimeout( function () {
-				shot( args.url, pageName, args.screen, args.out )
-			}, 200)
-		})
-	});
-}
+	})
